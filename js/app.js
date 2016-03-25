2d74958dfb0702115ab45d98f18b05622992cf8c
Tracy Wu <sytracywu@gmail.com>
2016-03-25 11:43:40 +0000

    Basic HTML and CSS to draw up grid of game

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//mines to appear in randomized coordinates 
+//set number of mines each game, and for every incremental game, increment the mines by 1
+//use x and y axis to draw coordinates and maths random to select the numbers between 0-2
+// create winning combination, which is to select square on all four sides of existing square ONLY, then avoiding the mines, go from start to finish
+
+console.log("hello")
